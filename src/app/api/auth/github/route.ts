40ba211ea6b1ae2992adc059827504d30234cbb0
kartikgupta666 <kartikgangil@gmail.com>
2026-03-24 15:26:02 +0530

    fix bug of jwt

--- src/app/api/auth/github/route.ts
+++ src/app/api/auth/github/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const baseURL = process.env.baseURL || 'http://localhost:3000'
+
 export function GET(req: NextRequest) {
     const clientId = process.env.GITHUB_CLIENT_ID;
 
     const redirectUrl =
-        `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user&redirect_uri=http://localhost:3000/api/auth/github/callback`
+        `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user&redirect_uri=${baseURL}/api/auth/github/callback`
 
     return NextResponse.redirect(redirectUrl);
 
